937765a5cbdbf0ef9aa45e3a2126610b18e93d08
domi <56755378+qazplm5602@users.noreply.github.com>
2021-01-17 00:37:39 +0900

    DomiEyes 0.0.1

---
+++ config.lua
@@ -0,0 +1,16 @@
+conifg = {}
+
+conifg.Lisence = ''
+
+conifg.scriptlist = {
+	tnotify = true,
+	dominotify = true
+}
+
+function domieyes_config()
+    if conifg ~= nil then
+        return conifg
+    else
+        return nil
+    end
+end
